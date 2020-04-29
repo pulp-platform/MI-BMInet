cf3fd12406b23b741a48f3a3e5d3b51a53aa9a11
Michael Hersche <herschmi@iis.ee.ethz.ch>
2020-04-29 14:42:20 +0200

    Layer freezing done

--- main_ss.py
+++ main_ss.py
@@ -63,7 +63,7 @@ def exclude_subjects(all_subjects=range(1,110), excluded_subjects=[88,92,100,104
 # Save results
 #
 #################################################
-def save_results(first_eval,tr_hist,num_classes,sub,split,n_ds,n_ch,T):
+def save_results(first_eval,tr_hist,num_classes,sub,split,n_ds,n_ch,T,fl):
 
     # Save metrics  
     results = np.zeros((4,1+len(tr_hist.history['acc'])))
@@ -79,14 +79,27 @@ def save_results(first_eval,tr_hist,num_classes,sub,split,n_ds,n_ch,T):
     results[3,1:] = tr_hist.history['val_loss']
 
     sub_str = '{0:03d}'.format(sub)
-    results_str = f'{results_dir}/stats/ss_class_{num_classes}_ds{n_ds}_nch{n_ch}_T{T}_sub{sub_str}_split_{split}.csv'
+    results_str = f'{results_dir}/stats/ss_class_{num_classes}_ds{n_ds}_nch{n_ch}_T{T}_fl{fl}_sub{sub_str}_split_{split}.csv'
     np.savetxt(results_str, np.transpose(results))
     return results[0:2,-1]
 
+def freezelayer(model, n_level, verbose=False):
+
+    num_layer = {0:0,1:3,2:8,3:13}
+
+    for layer in model.layers[:num_layer[n_level]]:
+        layer.trainable = False
+
+    if verbose:
+        for l in model.layers:
+            print(l.name, l.trainable)
+
+    return model
+
 ##############################################
 # CHANGE EXPERIMENT NAME FOR DIFFERENT TESTS!!
-ss_experiment = 'ss_headphone_eeg_CProw'
-global_experiment = 'headphone_eeg_CProw'
+ss_experiment = 'ss_freezlayer'
+global_experiment = 'headphone_eeg'
 ##############################################
 datapath = "/usr/scratch/xavier/herschmi/EEG_data/physionet/"
 
@@ -105,64 +118,67 @@ n_subjects = len(subjects)
 n_ds = 1
 T = 3
 #n_ch = 64
-n_ch_list = [4,6,10,14,18,20]#[2,3,5,7,9,11,64] # number of channels {8,19,27,38,64}
+n_ch_list = [64]#[4,6,10,14,18,20]#[2,3,5,7,9,11,64] # number of channels {8,19,27,38,64}
 verbose = 0 # verbosity for data loader and keras: 0 minimum, 
 
 # retraining parameters
 n_epochs = 10
 lr = 1e-3
+n_level_list = [0,1,2,3]
 
 for num_classes in num_classes_list:
-    for n_ch in n_ch_list:
-        # using 5 folds
-        num_splits = 5
-        kf_global = KFold(n_splits = num_splits)
-        
-        split_ctr = 0
+    for n_level in n_level_list:
+        for n_ch in n_ch_list:
+            # using 5 folds
+            num_splits = 5
+            kf_global = KFold(n_splits = num_splits)
+            
+            split_ctr = 0
 
-        acc = np.zeros((n_subjects,4,2))
+            acc = np.zeros((n_subjects,4,2))
 
-        # run over 5 global folds 
-        for _, test_sub_global in kf_global.split(subjects):
-            for sub_idx in test_sub_global:
-                
-                subject = subjects[sub_idx]
-                X_sub, y_sub = get.get_data(datapath, n_classes=num_classes, subjects_list=[subject])
-
-                # reduce EEG data (downsample, number of channels, time window)
-                X_sub = eeg_reduction(X_sub,n_ds = n_ds, n_ch = n_ch, T = T)
-
-                X_sub = np.expand_dims(X_sub,  axis=-1)
-                y_sub_cat = np_utils.to_categorical(y_sub)
-                n_samples = np.shape(X_sub)[2]
-                # split data while balancing classes 
-                kf_subject = StratifiedKFold(n_splits=4, shuffle=True, random_state=42)
-                sub_split_ctr = 0
-
-                for train_sub, test_sub in kf_subject.split(X_sub, y_sub):
+            # run over 5 global folds 
+            for _, test_sub_global in kf_global.split(subjects):
+                for sub_idx in test_sub_global:
                     
-                    # load global model
-                    model = load_model(global_model_path+f'global_class_{num_classes}_ds{n_ds}_nch{n_ch}_T{T}_split_{split_ctr}.h5')
-                    first_eval = model.evaluate(X_sub[test_sub], y_sub_cat[test_sub], batch_size=16, verbose = verbose) 
-                    
-                    adam_alpha = Adam(lr=lr)
-                    model.compile(loss='categorical_crossentropy', optimizer=adam_alpha, metrics = ['accuracy'])
-                    # creating a history object
-                    history = model.fit(X_sub[train_sub], y_sub_cat[train_sub], 
-                            validation_data=(X_sub[test_sub], y_sub_cat[test_sub]),
-                            batch_size = 16, epochs = n_epochs,  verbose = verbose) # callbacks=[lrate]
-                    
-                    # save results
-                    acc[sub_idx,sub_split_ctr]=save_results(first_eval,history,num_classes,subject,sub_split_ctr,n_ds,n_ch,T)
+                    subject = subjects[sub_idx]
+                    X_sub, y_sub = get.get_data(datapath, n_classes=num_classes, subjects_list=[subject])
 
-                    K.clear_session()
-                    sub_split_ctr = sub_split_ctr + 1
-               
-                print("S{:d}\t{:.4f}\t{:.4f}".format(subject,acc[sub_idx,:,0].mean(),acc[sub_idx,:,1].mean()))
+                    # reduce EEG data (downsample, number of channels, time window)
+                    X_sub = eeg_reduction(X_sub,n_ds = n_ds, n_ch = n_ch, T = T)
+
+                    X_sub = np.expand_dims(X_sub,  axis=-1)
+                    y_sub_cat = np_utils.to_categorical(y_sub)
+                    n_samples = np.shape(X_sub)[2]
+                    # split data while balancing classes 
+                    kf_subject = StratifiedKFold(n_splits=4, shuffle=True, random_state=42)
+                    sub_split_ctr = 0
+
+                    for train_sub, test_sub in kf_subject.split(X_sub, y_sub):
+                        
+                        # load global model
+                        model = load_model(global_model_path+f'global_class_{num_classes}_ds{n_ds}_nch{n_ch}_T{T}_split_{split_ctr}.h5')
+                        first_eval = model.evaluate(X_sub[test_sub], y_sub_cat[test_sub], batch_size=16, verbose = verbose) 
+
+                        model = freezelayer(model, n_level)
+                        
+                        adam_alpha = Adam(lr=lr)
+                        model.compile(loss='categorical_crossentropy', optimizer=adam_alpha, metrics = ['accuracy'])
+                        # creating a history object
+                        history = model.fit(X_sub[train_sub], y_sub_cat[train_sub], 
+                                validation_data=(X_sub[test_sub], y_sub_cat[test_sub]),
+                                batch_size = 16, epochs = n_epochs,  verbose = verbose) # callbacks=[lrate]
+                        # save results
+                        acc[sub_idx,sub_split_ctr]=save_results(first_eval,history,num_classes,subject,sub_split_ctr,n_ds,n_ch,T,n_level)
+
+                        K.clear_session()
+                        sub_split_ctr = sub_split_ctr + 1
+                   
+                    print("S{:d}\t{:.4f}\t{:.4f}".format(subject,acc[sub_idx,:,0].mean(),acc[sub_idx,:,1].mean()))
 
 
-            split_ctr = split_ctr + 1
+                split_ctr = split_ctr + 1
 
 
-        print("AVG\t{:.4f}\t{:.4f}".format(acc[:,:,0].mean(),acc[:,:,1].mean()))
+            print("AVG\t{:.4f}\t{:.4f}".format(acc[:,:,0].mean(),acc[:,:,1].mean()))
 
